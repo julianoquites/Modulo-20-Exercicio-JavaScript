138f13f959be561d7a4a1080b929588afd7fe88c
Juliano Quites <juliano.quites@gmail.com>
2024-08-30 16:12:48 -0300

    upload do exercicio

---
+++ mdc.js
@@ -0,0 +1,12 @@
+//1. Calcule o MDC (máximo divisor comum) entre dois números.
+const mdc = (a, b) => {
+  if (b === 0) {
+    return a;
+  } else {
+    return mdc(b, a % b);
+  }
+};
+
+console.log(mdc(20, 15));
+
+module.exports = { mdc };
