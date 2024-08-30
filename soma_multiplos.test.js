118990fd3085a1e19fd231e02f1d33412f0c9466
Juliano Quites <juliano.quites@gmail.com>
2024-08-30 16:15:36 -0300

    upload do exercicio

--- soma_multiplos.test.js
+++ soma_multiplos.test.js
@@ -1,6 +1,6 @@
 const { somaMultiplosDe5ou7 } = require("./soma_multiplos");
 
-describe("Soma de Multiplos", () => {
+describe("Soma de Multiplos de 5 ou 7", () => {
 it("Abaixo de 1000", () => {
         expect(somaMultiplosDe5ou7(1000)).toBe(156361);
     });
